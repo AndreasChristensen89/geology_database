0ad576b1ab8cab5b78bc65fefc0c8634da225f07
AndreasChristensen89 <brogaard89@gmail.com>
2022-10-19 12:30:51 +0000

    Add view, url, template, and css

--- INTERFACE/urls.py
+++ INTERFACE/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'INTERFACE'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('data-display', views.data_display, name='data_display')
 ]
--- INTERFACE/views.py
+++ INTERFACE/views.py
@@ -4,3 +4,9 @@ def index(request):
     template = "INTERFACE/index.html"
 
     return render(request, template)
+
+
+def data_display(request):
+    template = 'INTERFACE/data_display.html'
+
+    return render(request, template)
